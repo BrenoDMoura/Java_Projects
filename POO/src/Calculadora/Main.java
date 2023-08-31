9804cc5c855938f97bf0badf7c5fea3d0a50fcdf
BrenoDMoura <brenobatista1990@gmail.com>
2023-08-31 19:46:39 -0300

    Oriented Objects Programming

---
+++ POO/src/Calculadora/Main.java
@@ -0,0 +1,12 @@
+package pinto;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        String minhapika= input.nextLine();
+        System.out.println(minhapika);
+
+        input.close();
+    }
+}
